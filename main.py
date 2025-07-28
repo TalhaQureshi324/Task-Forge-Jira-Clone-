999ba51eeed1fb21f59d83a992ce04301a903e52
Muhammad Talha Qureshi <iamtalhaqureshi849@gmail.com>
2025-07-28 14:54:02 +0500

    secondary entry point.

---
+++ main.py
@@ -0,0 +1,34 @@
+﻿# # main.py
+
+# from menu import login_user, signup_user, dashboard
+
+# def main_menu():
+#     while True:
+#         print("\n Welcome to JIRA Clone Console")
+#         print("1. Login")
+#         print("2. Sign Up")
+#         print("0. Exit")
+
+#         choice = input("Select option: ")
+#         if choice == '1':
+#             email = input("Email: ")
+#             password = input("Password: ")
+#             role = login_user(email, password)
+#             if role:
+#                 dashboard(email, role)
+#             else:
+#                 print(" Invalid login.")
+#         elif choice == '2':
+#             signup_user()
+#         elif choice == '0':
+#             print(" Exiting...")
+#             break
+#         else:
+#             print(" Invalid choice.")
+
+# if __name__ == "__main__":
+#     try:
+#         main_menu()
+#     except Exception as e:
+#         print(f" Unexpected Error: {e}")
+#         input("Press Enter to exit...")
